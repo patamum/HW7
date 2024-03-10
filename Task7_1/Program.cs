6d57cc9dadbfa27b8517fcb34c5ce2c173a458ee
Svetlana Jourkova <belkina_s@mail.ru>
2024-03-10 23:27:46 +0300

    Добавьте файлы проекта.

---
+++ Task7_1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task7_1
+{
+    internal class Program
+    {
+        static bool IsTriangle(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && c + b > a)
+                return true;
+            return false;
+        }
+        static double GetSquare(double a, double b, double c)
+        {
+            double pp = (a + b + c) / 2; ;
+            return Math.Sqrt(pp * (pp - a) * (pp - b) * (pp - c));
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины строн первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            if (!IsTriangle(x1, x2, x3))
+            {
+                Console.WriteLine("Треугольник со стронами {0}, {1}, {2} не существует", x1, x2, x3);
+                return;
+            }
+            double s1 = GetSquare(x1, x2, x3);
+            
+            Console.WriteLine("Введите длины строн второго треугольника");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            if (!IsTriangle(y1, y2, y3))
+            {
+                Console.WriteLine("Треугольник со стронами {0}, {1}, {2} не существует", y1, y2, y3);
+                return;
+            }
+            double s2 = GetSquare(y1, y2, y3);
+            Console.WriteLine("{0:.00}, {1:.00}", s1, s2);                                              
+            if (s1 > s2)
+                Console.WriteLine("Площадь первого треугольника больше");
+            else if (s1 < s2)
+                Console.WriteLine("Площадь второго треугольника больше");
+            else 
+                Console.WriteLine("Площади равны");
+        }
+    }
+}
---
+++ Task7_2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task7_2
+{
+    internal class Program
+    {
+        static void CalcCube(double a, out double s, out double v)
+        {
+            s = 6 * a * a;
+            v = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            CalcCube(a, out s, out v);
+            Console.WriteLine("площадь поверхности куба {0:.00} объем {1:.00}", s, v);
+        }
+    }
+}
